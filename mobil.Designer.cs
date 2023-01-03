0dd92412c2544f4ca26163f54d342c46fc5b1a81
FadilAfza08 <120709227+FadilAfza08@users.noreply.github.com>
2023-01-04 00:10:21 +0700

    Add files via upload

---
+++ mobil.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace login
+{
+    partial class mobil
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btndeletemobil = new System.Windows.Forms.Button();
+            this.btnupdatemobil = new System.Windows.Forms.Button();
+            this.btnsavemobil = new System.Windows.Forms.Button();
+            this.txthargasewa = new System.Windows.Forms.TextBox();
+            this.Label6 = new System.Windows.Forms.Label();
+            this.txtmerkmobil = new System.Windows.Forms.TextBox();
+            this.Label5 = new System.Windows.Forms.Label();
+            this.txtnamamobil = new System.Windows.Forms.TextBox();
+            this.Label4 = new System.Windows.Forms.Label();
+            this.txtunitmobil = new System.Windows.Forms.TextBox();
+            this.Label3 = new System.Windows.Forms.Label();
+            this.txtnoplat = new System.Windows.Forms.TextBox();
+            this.Label2 = new System.Windows.Forms.Label();
+            this.txtidmobil = new System.Windows.Forms.TextBox();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btndeletemobil
+            // 
+            this.btndeletemobil.Location = new System.Drawing.Point(313, 432);
+            this.btndeletemobil.Name = "btndeletemobil";
+            this.btndeletemobil.Size = new System.Drawing.Size(128, 35);
+            this.btndeletemobil.TabIndex = 29;
+            this.btndeletemobil.Text = "DELETE";
+            this.btndeletemobil.UseVisualStyleBackColor = true;
+            // 
+            // btnupdatemobil
+            // 
+            this.btnupdatemobil.Location = new System.Drawing.Point(165, 432);
+            this.btnupdatemobil.Name = "btnupdatemobil";
+            this.btnupdatemobil.Size = new System.Drawing.Size(128, 35);
+            this.btnupdatemobil.TabIndex = 28;
+            this.btnupdatemobil.Text = "UPDATE";
+            this.btnupdatemobil.UseVisualStyleBackColor = true;
+            // 
+            // btnsavemobil
+            // 
+            this.btnsavemobil.Location = new System.Drawing.Point(23, 432);
+            this.btnsavemobil.Name = "btnsavemobil";
+            this.btnsavemobil.Size = new System.Drawing.Size(128, 35);
+            this.btnsavemobil.TabIndex = 27;
+            this.btnsavemobil.Text = "SAVE";
+            this.btnsavemobil.UseVisualStyleBackColor = true;
+            // 
+            // txthargasewa
+            // 
+            this.txthargasewa.Location = new System.Drawing.Point(244, 377);
+            this.txthargasewa.Name = "txthargasewa";
+            this.txthargasewa.Size = new System.Drawing.Size(171, 26);
+            this.txthargasewa.TabIndex = 26;
+            // 
+            // Label6
+            // 
+            this.Label6.AutoSize = true;
+            this.Label6.Location = new System.Drawing.Point(51, 383);
+            this.Label6.Name = "Label6";
+            this.Label6.Size = new System.Drawing.Size(120, 20);
+            this.Label6.TabIndex = 25;
+            this.Label6.Text = "HARGA SEWA";
+            // 
+            // txtmerkmobil
+            // 
+            this.txtmerkmobil.Location = new System.Drawing.Point(244, 320);
+            this.txtmerkmobil.Name = "txtmerkmobil";
+            this.txtmerkmobil.Size = new System.Drawing.Size(171, 26);
+            this.txtmerkmobil.TabIndex = 24;
+            // 
+            // Label5
+            // 
+            this.Label5.AutoSize = true;
+            this.Label5.Location = new System.Drawing.Point(51, 326);
+            this.Label5.Name = "Label5";
+            this.Label5.Size = new System.Drawing.Size(109, 20);
+            this.Label5.TabIndex = 23;
+            this.Label5.Text = "MERK MOBIL";
+            // 
+            // txtnamamobil
+            // 
+            this.txtnamamobil.Location = new System.Drawing.Point(244, 257);
+            this.txtnamamobil.Name = "txtnamamobil";
+            this.txtnamamobil.Size = new System.Drawing.Size(171, 26);
+            this.txtnamamobil.TabIndex = 22;
+            // 
+            // Label4
+            // 
+            this.Label4.AutoSize = true;
+            this.Label4.Location = new System.Drawing.Point(51, 263);
+            this.Label4.Name = "Label4";
+            this.Label4.Size = new System.Drawing.Size(109, 20);
+            this.Label4.TabIndex = 21;
+            this.Label4.Text = "NAMA MOBIL";
+            // 
+            // txtunitmobil
+            // 
+            this.txtunitmobil.Location = new System.Drawing.Point(244, 194);
+            this.txtunitmobil.Name = "txtunitmobil";
+            this.txtunitmobil.Size = new System.Drawing.Size(171, 26);
+            this.txtunitmobil.TabIndex = 20;
+            // 
+            // Label3
+            // 
+            this.Label3.AutoSize = true;
+            this.Label3.Location = new System.Drawing.Point(51, 200);
+            this.Label3.Name = "Label3";
+            this.Label3.Size = new System.Drawing.Size(100, 20);
+            this.Label3.TabIndex = 19;
+            this.Label3.Text = "UNIT MOBIL";
+            // 
+            // txtnoplat
+            // 
+            this.txtnoplat.Location = new System.Drawing.Point(244, 134);
+            this.txtnoplat.Name = "txtnoplat";
+            this.txtnoplat.Size = new System.Drawing.Size(171, 26);
+            this.txtnoplat.TabIndex = 18;
+            // 
+            // Label2
+            // 
+            this.Label2.AutoSize = true;
+            this.Label2.Location = new System.Drawing.Point(51, 140);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(75, 20);
+            this.Label2.TabIndex = 17;
+            this.Label2.Text = "NO PLAT";
+            // 
+            // txtidmobil
+            // 
+            this.txtidmobil.Location = new System.Drawing.Point(244, 81);
+            this.txtidmobil.Name = "txtidmobil";
+            this.txtidmobil.Size = new System.Drawing.Size(171, 26);
+            this.txtidmobil.TabIndex = 16;
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Location = new System.Drawing.Point(51, 87);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(80, 20);
+            this.Label1.TabIndex = 15;
+            this.Label1.Text = "ID MOBIL";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(140, 31);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(163, 20);
+            this.label7.TabIndex = 30;
+            this.label7.Text = "REGISTRASI MOBIL";
+            // 
+            // mobil
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(466, 511);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.btndeletemobil);
+            this.Controls.Add(this.btnupdatemobil);
+            this.Controls.Add(this.btnsavemobil);
+            this.Controls.Add(this.txthargasewa);
+            this.Controls.Add(this.Label6);
+            this.Controls.Add(this.txtmerkmobil);
+            this.Controls.Add(this.Label5);
+            this.Controls.Add(this.txtnamamobil);
+            this.Controls.Add(this.Label4);
+            this.Controls.Add(this.txtunitmobil);
+            this.Controls.Add(this.Label3);
+            this.Controls.Add(this.txtnoplat);
+            this.Controls.Add(this.Label2);
+            this.Controls.Add(this.txtidmobil);
+            this.Controls.Add(this.Label1);
+            this.Name = "mobil";
+            this.Text = "mobil";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.Button btndeletemobil;
+        internal System.Windows.Forms.Button btnupdatemobil;
+        internal System.Windows.Forms.Button btnsavemobil;
+        internal System.Windows.Forms.TextBox txthargasewa;
+        internal System.Windows.Forms.Label Label6;
+        internal System.Windows.Forms.TextBox txtmerkmobil;
+        internal System.Windows.Forms.Label Label5;
+        internal System.Windows.Forms.TextBox txtnamamobil;
+        internal System.Windows.Forms.Label Label4;
+        internal System.Windows.Forms.TextBox txtunitmobil;
+        internal System.Windows.Forms.Label Label3;
+        internal System.Windows.Forms.TextBox txtnoplat;
+        internal System.Windows.Forms.Label Label2;
+        internal System.Windows.Forms.TextBox txtidmobil;
+        internal System.Windows.Forms.Label Label1;
+        internal System.Windows.Forms.Label label7;
+    }
+}
