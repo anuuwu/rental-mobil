1e78ef355ddca7a731ac8a157269a3902de68acc
isxxlmvh17 <120400086+isxxlmvh17@users.noreply.github.com>
2023-01-04 00:13:55 +0700

    Add files via upload

---
+++ pelanggan.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace login
+{
+    partial class pelanggan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.btndeletepelanggan = new System.Windows.Forms.Button();
+            this.btnupdatepelanggan = new System.Windows.Forms.Button();
+            this.btnsavepelanggan = new System.Windows.Forms.Button();
+            this.txtalamat = new System.Windows.Forms.TextBox();
+            this.Label4 = new System.Windows.Forms.Label();
+            this.txtnohp = new System.Windows.Forms.TextBox();
+            this.Label3 = new System.Windows.Forms.Label();
+            this.txtnama = new System.Windows.Forms.TextBox();
+            this.Label2 = new System.Windows.Forms.Label();
+            this.txtnik = new System.Windows.Forms.TextBox();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // btndeletepelanggan
+            // 
+            this.btndeletepelanggan.Location = new System.Drawing.Point(309, 335);
+            this.btndeletepelanggan.Name = "btndeletepelanggan";
+            this.btndeletepelanggan.Size = new System.Drawing.Size(128, 35);
+            this.btndeletepelanggan.TabIndex = 29;
+            this.btndeletepelanggan.Text = "DELETE";
+            this.btndeletepelanggan.UseVisualStyleBackColor = true;
+            // 
+            // btnupdatepelanggan
+            // 
+            this.btnupdatepelanggan.Location = new System.Drawing.Point(161, 335);
+            this.btnupdatepelanggan.Name = "btnupdatepelanggan";
+            this.btnupdatepelanggan.Size = new System.Drawing.Size(128, 35);
+            this.btnupdatepelanggan.TabIndex = 28;
+            this.btnupdatepelanggan.Text = "UPDATE";
+            this.btnupdatepelanggan.UseVisualStyleBackColor = true;
+            // 
+            // btnsavepelanggan
+            // 
+            this.btnsavepelanggan.Location = new System.Drawing.Point(19, 335);
+            this.btnsavepelanggan.Name = "btnsavepelanggan";
+            this.btnsavepelanggan.Size = new System.Drawing.Size(128, 35);
+            this.btnsavepelanggan.TabIndex = 27;
+            this.btnsavepelanggan.Text = "SAVE";
+            this.btnsavepelanggan.UseVisualStyleBackColor = true;
+            // 
+            // txtalamat
+            // 
+            this.txtalamat.Location = new System.Drawing.Point(207, 264);
+            this.txtalamat.Name = "txtalamat";
+            this.txtalamat.Size = new System.Drawing.Size(171, 26);
+            this.txtalamat.TabIndex = 22;
+            // 
+            // Label4
+            // 
+            this.Label4.AutoSize = true;
+            this.Label4.Location = new System.Drawing.Point(90, 270);
+            this.Label4.Name = "Label4";
+            this.Label4.Size = new System.Drawing.Size(73, 20);
+            this.Label4.TabIndex = 21;
+            this.Label4.Text = "ALAMAT";
+            // 
+            // txtnohp
+            // 
+            this.txtnohp.Location = new System.Drawing.Point(207, 201);
+            this.txtnohp.Name = "txtnohp";
+            this.txtnohp.Size = new System.Drawing.Size(171, 26);
+            this.txtnohp.TabIndex = 20;
+            // 
+            // Label3
+            // 
+            this.Label3.AutoSize = true;
+            this.Label3.Location = new System.Drawing.Point(90, 207);
+            this.Label3.Name = "Label3";
+            this.Label3.Size = new System.Drawing.Size(58, 20);
+            this.Label3.TabIndex = 19;
+            this.Label3.Text = "NO HP";
+            // 
+            // txtnama
+            // 
+            this.txtnama.Location = new System.Drawing.Point(207, 141);
+            this.txtnama.Name = "txtnama";
+            this.txtnama.Size = new System.Drawing.Size(171, 26);
+            this.txtnama.TabIndex = 18;
+            // 
+            // Label2
+            // 
+            this.Label2.AutoSize = true;
+            this.Label2.Location = new System.Drawing.Point(90, 147);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(55, 20);
+            this.Label2.TabIndex = 17;
+            this.Label2.Text = "NAMA\r\n";
+            // 
+            // txtnik
+            // 
+            this.txtnik.Location = new System.Drawing.Point(207, 88);
+            this.txtnik.Name = "txtnik";
+            this.txtnik.Size = new System.Drawing.Size(171, 26);
+            this.txtnik.TabIndex = 16;
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Location = new System.Drawing.Point(90, 94);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(35, 20);
+            this.Label1.TabIndex = 15;
+            this.Label1.Text = "NIK";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(169, 37);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(109, 20);
+            this.label5.TabIndex = 30;
+            this.label5.Text = "PELANGGAN";
+            // 
+            // pelanggan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(449, 446);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btndeletepelanggan);
+            this.Controls.Add(this.btnupdatepelanggan);
+            this.Controls.Add(this.btnsavepelanggan);
+            this.Controls.Add(this.txtalamat);
+            this.Controls.Add(this.Label4);
+            this.Controls.Add(this.txtnohp);
+            this.Controls.Add(this.Label3);
+            this.Controls.Add(this.txtnama);
+            this.Controls.Add(this.Label2);
+            this.Controls.Add(this.txtnik);
+            this.Controls.Add(this.Label1);
+            this.Name = "pelanggan";
+            this.Text = "pelanggan";
+            this.Load += new System.EventHandler(this.pelanggan_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        internal System.Windows.Forms.Button btndeletepelanggan;
+        internal System.Windows.Forms.Button btnupdatepelanggan;
+        internal System.Windows.Forms.Button btnsavepelanggan;
+        internal System.Windows.Forms.TextBox txtalamat;
+        internal System.Windows.Forms.Label Label4;
+        internal System.Windows.Forms.TextBox txtnohp;
+        internal System.Windows.Forms.Label Label3;
+        internal System.Windows.Forms.TextBox txtnama;
+        internal System.Windows.Forms.Label Label2;
+        internal System.Windows.Forms.TextBox txtnik;
+        internal System.Windows.Forms.Label Label1;
+        private System.Windows.Forms.Label label5;
+    }
+}
