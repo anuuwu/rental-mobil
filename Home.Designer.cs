cbb5c1bd2569fd52e425eb99acc5e89cd2a05f44
FadilAfza08 <120709227+FadilAfza08@users.noreply.github.com>
2023-01-06 15:48:03 +0700

    Add files via upload

--- Home.Designer.cs
+++ Home.Designer.cs
@@ -28,34 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnmobil = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.btnpelanggan = new System.Windows.Forms.Button();
+            this.btntrans = new System.Windows.Forms.Button();
+            this.btnpenyewaan = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // btnmobil
+            // 
+            this.btnmobil.Location = new System.Drawing.Point(53, 334);
+            this.btnmobil.Name = "btnmobil";
+            this.btnmobil.Size = new System.Drawing.Size(102, 41);
+            this.btnmobil.TabIndex = 2;
+            this.btnmobil.Text = "Mobil";
+            this.btnmobil.UseVisualStyleBackColor = true;
+            this.btnmobil.Click += new System.EventHandler(this.btnmobil_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(222, 177);
+            this.label1.Location = new System.Drawing.Point(274, 181);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(175, 20);
+            this.label1.Size = new System.Drawing.Size(79, 20);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Welcome To Affaachh?";
+            this.label1.Text = "Welcome ";
             // 
-            // label2
+            // btnpelanggan
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(240, 215);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(157, 20);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Welcome To Sahara ";
+            this.btnpelanggan.Location = new System.Drawing.Point(193, 334);
+            this.btnpelanggan.Name = "btnpelanggan";
+            this.btnpelanggan.Size = new System.Drawing.Size(102, 41);
+            this.btnpelanggan.TabIndex = 3;
+            this.btnpelanggan.Text = "Pelanggan";
+            this.btnpelanggan.UseVisualStyleBackColor = true;
+            this.btnpelanggan.Click += new System.EventHandler(this.btnpelanggan_Click);
+            // 
+            // btntrans
+            // 
+            this.btntrans.Location = new System.Drawing.Point(347, 334);
+            this.btntrans.Name = "btntrans";
+            this.btntrans.Size = new System.Drawing.Size(102, 41);
+            this.btntrans.TabIndex = 4;
+            this.btntrans.Text = "Transaksi";
+            this.btntrans.UseVisualStyleBackColor = true;
+            this.btntrans.Click += new System.EventHandler(this.btntrans_Click);
+            // 
+            // btnpenyewaan
+            // 
+            this.btnpenyewaan.Location = new System.Drawing.Point(498, 334);
+            this.btnpenyewaan.Name = "btnpenyewaan";
+            this.btnpenyewaan.Size = new System.Drawing.Size(102, 41);
+            this.btnpenyewaan.TabIndex = 5;
+            this.btnpenyewaan.Text = "Penyewaan";
+            this.btnpenyewaan.UseVisualStyleBackColor = true;
+            this.btnpenyewaan.Click += new System.EventHandler(this.btnpenyewaan_Click);
             // 
             // Home
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(622, 501);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnpenyewaan);
+            this.Controls.Add(this.btntrans);
+            this.Controls.Add(this.btnpelanggan);
+            this.Controls.Add(this.btnmobil);
             this.Controls.Add(this.label1);
             this.Name = "Home";
             this.Text = "Home";
@@ -66,7 +103,10 @@
 
         #endregion
 
+        private System.Windows.Forms.Button btnmobil;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnpelanggan;
+        private System.Windows.Forms.Button btntrans;
+        private System.Windows.Forms.Button btnpenyewaan;
     }
 }
---
+++ Home.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace login
+{
+    public partial class Home : Form
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void btnmobil_Click(object sender, EventArgs e)
+        {
+            mobil panggil = new mobil();
+            panggil.Show();
+        }
+
+        private void btnpelanggan_Click(object sender, EventArgs e)
+        {
+            pelanggan panggil = new pelanggan();
+            panggil.Show();
+        }
+
+        private void btntrans_Click(object sender, EventArgs e)
+        {
+            transaksi panggil = new transaksi();
+            panggil.Show();
+        }
+
+        private void btnpenyewaan_Click(object sender, EventArgs e)
+        {
+            penyewaan panggil = new penyewaan();
+            panggil.Show();
+        }     
+    }
+}
